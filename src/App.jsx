bbb4ffe852f6367901691574c15f3336e08fd2a5
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-06-12 20:23:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
-import AudioRecorder from './components/AudioRecorder';
-import AudioList from './components/AudioList';
+import AudioRecorder from "./components/AudioRecorder";
+import AudioList from "./components/AudioList";
+
+
 
 function App() {
   const [refresh, setRefresh] = useState(false);
+ const fetchAudioListAgain = () => {
+    setRefresh(prev => !prev);
+  };
+ 
 
   return (
     <div>
-      <h1>Audio Recorder</h1>
-      <AudioRecorder onUploadSuccess={() => setRefresh(!refresh)} />
-      <AudioList key={refresh} />
+    
+      <AudioRecorder onUploadSuccess={fetchAudioListAgain} />
+      <div className="list"><AudioList refresh={refresh} /> </div>
     </div>
   );
 }
